6427ea6c131ce6f42f2ca622a826ad0a66f80f9a
Andrew <kucoinethtrader@gmail.com>
2024-09-09 09:33:02 +1000

    Add zero density energy estimate python file

---
+++ blueprint/src/python/zero_density_energy_estimate.py
@@ -0,0 +1,8 @@
+# code for bounding the function A*(\sigma) in the bound
+#
+# N*(\sigma - \delta, T) \ll T^{A*(\sigma)(1 - \sigma) + o(1)}
+#
+# for all \delta > 0, where N(\sigma, T) is the additive energy of 
+# the imaginary parts of the zeroes \rho of the Riemann zeta-function 
+# satisfying with real part \in [\sigma, 1] and imaginary part \in 
+# [-T, T]. 
