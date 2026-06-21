6eb95dc61e3c7308b1ba5110cd12cb6fd43b472a
Sebastião de Abreu Cavalcante <sebastiaoabreu@gmail.com>
2026-06-21 17:42:32 -0300

    Register ep_to_zd regression test in test_all

--- blueprint/src/python/tests/test_all.py
+++ blueprint/src/python/tests/test_all.py
@@ -14,4 +14,7 @@ print("All Polytope test cases passed.")
 import test_region
 print("All Region test cases passed.")
 
+import test_ep_to_zd
+print("All ep_to_zd regression test cases passed.")
+
 print("All test cases passed.")
