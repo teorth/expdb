2932177a7a326988122e7c41e837eb19f66a18bd
Shehzad Hathi <shehzadhathi@outlook.com>
2026-08-02 00:51:24 +1000

    Added Euler--Maclaurin formula from another repo since it is not available in Mathlib yet

--- Expdb.lean
+++ Expdb.lean
@@ -3,3 +3,4 @@ module
 public import Expdb.Basic.AutomaticUniformity
 public import Expdb.ExponentialSums.ExponentSumGrowthNonAsymptotic
 public import Expdb.Fourier.L2Integral
+public import Expdb.Mathlib.EulerMaclaurin
