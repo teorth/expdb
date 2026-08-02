88c0108565955a1ca1a1b961a77598e0f354dda8
Shehzad Hathi <shehzadhathi@outlook.com>
2026-08-02 14:38:49 +1000

    Adding trivial bounds to main expdb module

--- Expdb.lean
+++ Expdb.lean
@@ -2,5 +2,6 @@ module
 
 public import Expdb.Basic.AutomaticUniformity
 public import Expdb.ExponentialSums.ExponentSumGrowthNonAsymptotic
+public import Expdb.ExponentialSums.TrivialBounds
 public import Expdb.Fourier.L2Integral
 public import Expdb.Mathlib.EulerMaclaurin
