895c833fbe970665bb5090ecabbea9a41a920584
Shehzad Hathi <shehzadhathi@outlook.com>
2026-08-03 17:39:05 +1000

    Updated public imports in Expdb.lean

--- Expdb.lean
+++ Expdb.lean
@@ -2,6 +2,8 @@ module
 
 public import Expdb.Basic.AutomaticUniformity
 public import Expdb.ExponentialSums.ExponentSumGrowthNonAsymptotic
+public import Expdb.ExponentialSums.LogPhase
+public import Expdb.ExponentialSums.OscillatoryBounds
 public import Expdb.ExponentialSums.TrivialBounds
 public import Expdb.Fourier.L2Integral
 public import Expdb.Mathlib.EulerMaclaurin
